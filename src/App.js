82ac61eed05d162b4e7fe2d16e76ca48f9060405
suvadrashrestha <jeenashrestha04@gmail.com>
2023-09-01 19:50:02 +0545

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 
 import './App.css';
 import Variants from './components/skeleton';
-// import Slider from './components/Slider';
 function App() {
   return (<>
-   {/* <Slider></Slider> */}
    <Variants></Variants>
     </>
   );
